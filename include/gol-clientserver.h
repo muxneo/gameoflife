191384766ba62c82aab5c411c96491f5656e72bf
Mrukant Popat <mrukant@gmail.com>
2019-10-14 21:15:42 -0700

    -Working socket connections. WIP

--- include/gol-clientserver.h
+++ include/gol-clientserver.h
@@ -11,30 +11,66 @@
 #include <string.h>
 #include <pthread.h>
 #include <stdbool.h> /* bool datatype */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h> /* read, write functions */
+#include <stdlib.h> /* malloc */
+
+
+#define GOLSOCKET "/tmp/golsocket"
+#define COMMANDSIZE 20
+
+/* Shared data between client threads - start */
+int gen = 0;
+/* Shared data with thread - end   */
+
+/**
+@brief send_command_srv sends command entered by user to server
+*/
+void send_command_srv(int client_socket, const char* command);
+
+/**
+@brief connect_to_server socket connection to initiate connection to server
+*/
+int connect_to_server();
 
 
 /**
-@brief starts client logic
+@brief print_help prints list of commands for user to use
+*/
+void print_help();
+
+/**
+@brief start_client starts client logic
 */
 void start_client();
 
 /**
-@brief server thread to calculate matrix
+@brief client_matrix_thread server thread to calculate matrix
 */
 void* client_matrix_thread(void* thread_args);
 
 
 /**
-@brief starts server logic
+@brief start_server starts server logic
 */
 void start_server();
 
 /**
-@brief server thread to calculate matrix
+@brief server_matrix_thread server thread to calculate matrix
 */
 void* server_matrix_thread(void* thread_args);
 
+/**
+@brief connect_to_client server-side socket connection
+*/
+int connect_to_client();
 
 
+/**
+@brief read_client_command read command sent by client
+*/
+void read_client_command(int client_sockfd_srv, char* command);
 
 #endif /*_GOL_SERVER_HEADER_*/
--- src/gol-clientserver.c
+++ src/gol-clientserver.c
@@ -1,5 +1,5 @@
 /**
- * @file gol-server.c
+ * @file gol-clientserver.c
  * @author Mrukant Popat
  * @brief Game Of Life server implementation file
  */
@@ -7,18 +7,53 @@
 #include "gol-clientserver.h"
 
 
+void print_help(){
+  printf("\n");
+  printf("Client usage:\n");
+  printf(" h  - prints this help\n");
+  printf(" 4D - flips (kills or ressurects) cell at row 4 column D\n");
+  printf(" .  - retrieves and prints current state of the board\n");
+  printf(" o  - Init board with TOAD\n");
+  printf(" n  - Init board with BEACON\n");
+  printf(" i  - Init board with BLINKER\n");
+  printf(" r  - Init board with RANDOM\n");
+  printf(" s  - Executes single-setp\n");
+  printf(" g  - Go run server continuously executing generation steps\n");
+  printf(" t  - Stop\n");
+  printf(" q  - Quit and disconnect from server(Leaving it running)\n");
+  printf(" k  - Quit and kill server\n\n");
+  //  printf(" Client is connected to port %d",port);
+  printf(" %d>",gen);
+}
+
+int connect_to_server(){
+  int client_sock_fd;
+  struct sockaddr_un clntsockinfo;
+
+  /* create socket */
+  client_sock_fd =  socket(PF_LOCAL,SOCK_STREAM,0);
+
+  clntsockinfo.sun_family = PF_LOCAL;
+  strcpy(clntsockinfo.sun_path,GOLSOCKET);
+  connect(client_sock_fd, (struct sockaddr*) &clntsockinfo, SUN_LEN(&clntsockinfo));
+
+  return client_sock_fd;
+}
 
 void* client_matrix_thread(void* thread_args){
-  printf("\n CLIENT MATRIX THREAD\n");
-
+  //printf("\n CLIENT MATRIX THREAD\n");
   while(true){
-    
+   
   }
 }
 
-void start_client(){
-  printf("\n CLIENT \n");
+void send_command_srv(int client_socket, const char* command){
+  int comm_len = strlen(command) + 1;
+  write(client_socket, &comm_len, sizeof(comm_len)); /* send length */
+  write(client_socket, &command, comm_len);
+}
 
+void start_client(){
   /*
    - create matrix print thread
    - main thread - create socket and connect to server
@@ -26,13 +61,24 @@ void start_client(){
    - matrix thread - wait for data from server and print
    */
 
+  char command[COMMANDSIZE];
+  int client_sockfd;
+  
   pthread_t matrixcthread_id;
   pthread_create(&matrixcthread_id, NULL, &client_matrix_thread, NULL);
 
+  client_sockfd = connect_to_server();
+  
+  print_help();
+  
   while(true){
+    scanf("%s",command);
+    send_command_srv(client_sockfd, command);
   }
   
 }
+
+
 
 
 
@@ -45,8 +91,53 @@ void* server_matrix_thread(void* thread_args){
   }
 }
 
+
+int connect_to_client(){
+  int server_sock_fd;
+  int client_sockfd_accept;
+  struct sockaddr_un srvsockinfo;
+  struct sockaddr_un clntsockinfo_srv;
+  socklen_t clntsockinfosrv_len;
+
+  /* create socket */
+  server_sock_fd =  socket(PF_LOCAL,SOCK_STREAM,0);
+
+  srvsockinfo.sun_family = PF_LOCAL;
+  strcpy(srvsockinfo.sun_path,GOLSOCKET);
+  bind(server_sock_fd, (struct sockaddr*) &srvsockinfo, SUN_LEN(&srvsockinfo));
+  listen(server_sock_fd,1);
+  
+  client_sockfd_accept = accept(server_sock_fd, (struct sockaddr*) &clntsockinfo_srv, &clntsockinfosrv_len);
+
+  return client_sockfd_accept;
+}
+
+
+void read_client_command(int client_sockfd_srv, char* command){
+  int len = 0;
+  char* comm;
+  
+  while(true){
+    
+    if(read(client_sockfd_srv, &len, sizeof(len)) == 0){
+      printf("read len ");
+      return;
+    }
+    if(len !=0) printf("len = %d\n",len);
+    comm = (char*) malloc(len);
+    
+    read(client_sockfd_srv, comm, len);
+  }
+  printf("\n SERVER received %s\n", comm);
+  free(comm);
+}
+
+
 void start_server(){
   printf("\n SERVER \n");
+  int client_sockfd_srv;
+  char command[COMMANDSIZE];
+  
 
   /*
     - create matrix calc thread
@@ -57,8 +148,12 @@ void start_server(){
   pthread_t matrixsthread_id;
   pthread_create(&matrixsthread_id, NULL, &server_matrix_thread, NULL);
 
-  while(true){
-  }
+  client_sockfd_srv = connect_to_client();
+  
+  //while(true){
+    read_client_command(client_sockfd_srv, command);
+    
+    //}
 }
 
 
